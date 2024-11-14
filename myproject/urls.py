ad11a54d9150c7dd06f042603b07d4d24f724622
yada-code <yada.yk2555@gmail.com>
2024-11-14 14:10:14 +0700

    Add homepage view and URL

--- myproject/urls.py
+++ myproject/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from myapi import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('myapi.urls')),
-    path('', views.home, name='home'),
+
 ]
