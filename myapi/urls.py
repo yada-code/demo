8c4c4d11bdb3af0adf49db09114b264593762e99
yada-code <yada.yk2555@gmail.com>
2024-11-14 14:32:18 +0700

    Update project files

--- myapi/urls.py
+++ myapi/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),  # URL ที่ว่างจะเรียกใช้ view 'home'
     path('receive-payload/', views.receive_payload, name='receive_payload'),
-ฟก
+
     ]
